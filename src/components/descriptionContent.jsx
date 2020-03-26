db42d5ed6f17ebb5f3f9efaab0f49bd217645948
Mohammad Daffa <daffa@alterra.id>
2020-03-26 07:34:32 +0700

    refactoring description content component and remove unnecessary code

--- src/components/descriptionContent.jsx
+++ src/components/descriptionContent.jsx
@@ -24,37 +24,5 @@ const DescriptionContent = props => {
     </div>
   );
 };
-// class DescriptionContent extends Component {
-//   render() {
-//     return (
-//       <div
-//         className={this.props.className}
-//         onAnimationEnd={this.props.handleEndAnimation}
-//       >
-//         <div className="place-pict">
-//           <img
-//             className="img-fluid"
-//             src={this.props.imageUrl}
-//             alt={this.props.placeName}
-//           />
-//         </div>
-//         <div className="place-title">
-//           <h6 className="py-2 px-4">{this.props.placeName}</h6>
-//         </div>
-//         <div className="place-description">
-//           <p className="short-description pt-1 px-4">
-//             {this.props.shortDescription}
-//           </p>
-//           <p className="long-description px-4">{this.props.longDescription}</p>
-//         </div>
-//         <div className="place-information px-4">
-//           <span>{this.props.address}</span>
-//           <br />
-//           <span>{this.props.website}</span>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 export default DescriptionContent;
