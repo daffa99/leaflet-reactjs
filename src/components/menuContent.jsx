75ca980004f112d6103db59238570f29b007eb52
Mohammad Daffa <daffa@alterra.id>
2020-03-26 22:14:34 +0700

    add extra places for menu sidebar

--- src/components/menuContent.jsx
+++ src/components/menuContent.jsx
@@ -9,27 +9,53 @@ import data from "../data.json";
 const MenuContent = props => {
   // Map all places and its information from data (json)
   let listPlaces = data.map(item => {
+    if (item.extra_places.length > 0) {
+      let extraPlaces = item.extra_places.map(place => {
+        return <li className="py-1 pl-4">{place}</li>;
+      });
+      return (
+        <React.Fragment>
+          <button
+            type="button"
+            className={
+              props.clickedPlace === item.place_name
+                ? "btn btn-dark clicked"
+                : "btn btn-dark"
+            }
+            onClick={() => props.handlePlace(item)}
+          >
+            <li className="py-2">{item.place_name}</li>
+          </button>
+          <a href={`#${item.id}`} data-toggle="collapse">
+            <i class="fa fa-fw fa-sort-down"></i>
+          </a>
+          <div className="collapse" id={item.id}>
+            <ul className="extra-places list-unstyled">{extraPlaces}</ul>
+          </div>
+        </React.Fragment>
+      );
+    }
     return (
-      <button
-        type="button"
+      <a
+        href={`#${item.id}`}
         className={
           props.clickedPlace === item.place_name
-            ? "btn btn-dark clicked"
-            : "btn btn-dark"
+            ? "btn btn-dark no-extra clicked"
+            : "btn btn-dark no-extra"
         }
         onClick={() => props.handlePlace(item)}
       >
         <li className="py-2">{item.place_name}</li>
-      </button>
+      </a>
     );
   });
   return (
     <React.Fragment>
-      <div className="sidebar-menu px-4">
-        <div className="dropdown-filter-header py-5">
+      <div className="sidebar-menu pl-4 pr-2">
+        <div className="dropdown-filter-header px-2 py-4">
           <select id="filter-by">
-            <option value="favorite">filter by favorite</option>
-            <option value="none">filter by none</option>
+            <option value="favorite">Filter by favorite</option>
+            <option value="none">Filter by none</option>
           </select>
         </div>
         <ul className="list-place list-unstyled font-weight-bold">
